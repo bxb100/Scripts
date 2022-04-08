61cc545ed044609768f4e42c69b33a42702d11b6
Lemon <bxb100@outlook.com>
2022-04-08 19:25:05 +0800

    Update script.user.js

--- ddrk/script.user.js
+++ ddrk/script.user.js
@@ -1,16 +1,105 @@
 // ==UserScript==
 // @name         ddrk广告屏蔽
 // @namespace    com.pers.scripts
-// @version      1.0.11
+// @version      1.0.12
 // @description  临时屏蔽
 // @author       Johnbi
-// @date         2021-10-14
 // @run-at document-start
 // @match        https://ddrk.me/*
 // @icon         https://ddrk.me/favicon-32x32.png
-// @require https://greasyfork.org/scripts/434057-toolkitcollection/code/ToolKitCollection.js?version=980513
 // @grant        none
 // ==/UserScript==
+
+(() => {
+    // Library code, licensed under MIT
+    // https://github.com/jspenguin2017/Snippets/blob/master/onbeforescriptexecute.html
+
+    "use strict";
+
+    const Event = class {
+        constructor(script, target) {
+            this.script = script;
+            this.target = target;
+            // copy
+            this.target.src = this.script.src;
+
+            this._cancel = false;
+            this._replace = null;
+            this._stop = false;
+        }
+
+        preventDefault() {
+            this._cancel = true;
+        }
+        stopPropagation() {
+            this._stop = true;
+        }
+        replacePayload(payload) {
+            this._replace = payload;
+        }
+    };
+
+    let callbacks = [];
+    Window.prototype.addBeforeScriptExecuteListener = (f) => {
+        if (typeof f !== "function") {
+            throw new Error("Event handler must be a function.");
+        }
+        callbacks.push(f);
+    };
+    Window.prototype.removeBeforeScriptExecuteListener = (f) => {
+        let i = callbacks.length;
+        while (i--) {
+            if (callbacks[i] === f) {
+                callbacks.splice(i, 1);
+            }
+        }
+    };
+
+    const dispatch = (script, target) => {
+        if (script.tagName !== "SCRIPT") {
+            return;
+        }
+
+        const e = new Event(script, target);
+
+        if (typeof window.onbeforescriptexecute === "function") {
+            try {
+                window.onbeforescriptexecute(e);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        for (const func of callbacks) {
+            if (e._stop) {
+                break;
+            }
+            try {
+                func(e);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        if (e._cancel) {
+            script.textContent = "";
+            script.remove();
+        } else if (typeof e._replace === "string") {
+            script.textContent = e._replace;
+        }
+    };
+    const observer = new MutationObserver((mutations) => {
+        for (const m of mutations) {
+            for (const n of m.addedNodes) {
+                dispatch(n, m.target);
+            }
+        }
+    });
+    observer.observe(document, {
+        childList: true,
+        subtree: true,
+    });
+})();
 
 /*global ToolKitCollection*/
 function hideElement(id) {
@@ -34,10 +123,9 @@ function appendCss() {
 (function () {
     'use strict';
 
-    ToolKitCollection.beforescriptexecute((event) => {
+    window.addBeforeScriptExecuteListener((event) => {
         const src = event?.target?.src;
         if (src && src.search(/wp-playlist.min\.js/) != -1) {
-            ToolKitCollection.description();
             event.preventDefault();
             event.stopPropagation();
             append();
@@ -51,6 +139,7 @@ function appendCss() {
         hideElement("kasjbgih");
         hideElement("iaujwnefhw");
         hideElement("fkasjgf");
+        hideElement("fh972w3h4g3y9g");
     });
 
 })();
