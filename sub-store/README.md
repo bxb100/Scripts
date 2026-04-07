b5ff13bbe3f2c35dd284caa59c19df9b95a5c4dc
Lemon <20685961+bxb100@users.noreply.github.com>
2026-04-07 22:13:57 +0800

    chore: add doc

---
+++ sub-store/README.md
@@ -0,0 +1,4 @@
+# Resources
+
+- <https://linux.do/t/topic/851874>
+- <https://github.com/sub-store-org/Sub-Store/blob/master/scripts/demo.js>
--- sub-store/surge.js
+++ sub-store/surge.js
@@ -1,3 +1,5 @@
+//https://raw.githubusercontent.com/bxb100/Scripts/refs/heads/main/sub-store/surge.js#name=订阅名称
+
 const {name} = $arguments
 
 let surgeProxies = await produceArtifact({
@@ -6,4 +8,4 @@ let surgeProxies = await produceArtifact({
     platform: 'SurgeMac'
 })
 
-$content = `[General]\n\n[Rule]\nFINAL,DIRECT\n\n[Proxy]\n${surgeProxies}`
+$content = `[Proxy]\n${surgeProxies}\n\n[Rule]\nFINAL,DIRECT`
