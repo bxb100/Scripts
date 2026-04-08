067c648d573e15c9b3e811dca9ba50ae98317358
Lemon <20685961+bxb100@users.noreply.github.com>
2026-04-08 20:53:51 +0800

    feat: add surge managed config header

--- sub-store/surge.js
+++ sub-store/surge.js
@@ -1,11 +1,13 @@
-//https://raw.githubusercontent.com/bxb100/Scripts/refs/heads/main/sub-store/surge.js#name=订阅名称
+//https://raw.githubusercontent.com/bxb100/Scripts/refs/heads/main/sub-store/surge.js#name=订阅名称&remote=https%3A%2F%2Fsub.store%2Fapi%2Ffile%2Fikuuu-remote
 
-const {name} = $arguments
+const {name, remote} = $arguments
+
+const managed_config = remote ? remote : `https://sub.store/api/file/${name}-remote`
 
 let surgeProxies = await produceArtifact({
-    type: 'subscription',
-    name,
-    platform: 'SurgeMac'
+  type: 'subscription',
+  name,
+  platform: 'SurgeMac'
 })
 
-$content = `[Proxy]\n${surgeProxies}\n\n[Rule]\nFINAL,DIRECT`
+$content = `#!MANAGED-CONFIG ${managed_config} interval=43200 strict=true\n[Proxy]\n${surgeProxies}\n\n[Rule]\nFINAL,DIRECT`
